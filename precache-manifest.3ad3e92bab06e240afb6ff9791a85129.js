937878468fb1f38b4f966e06114588344502791e
aniket965 <aniket965.as@gmail.com>
2019-05-08 16:41:38 +0530

    Vcg: Update live demo

---
+++ precache-manifest.3ad3e92bab06e240afb6ff9791a85129.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "6cd5b4af663be8ca2cdb",
+    "url": "/css/app.a57109e3.css"
+  },
+  {
+    "revision": "6cd5b4af663be8ca2cdb",
+    "url": "/js/app.1629760c.js"
+  },
+  {
+    "revision": "208c2e4f8a3fcdd5233e",
+    "url": "/js/chunk-vendors.1e04a77a.js"
+  },
+  {
+    "revision": "c446e0d03920403f3c5dea9efd4d1821",
+    "url": "/img/1.c446e0d0.png"
+  },
+  {
+    "revision": "b59622539fff7f91a08ac8af4cd07b46",
+    "url": "/img/back.b5962253.jpg"
+  },
+  {
+    "revision": "ea963a2d0f4a1f867e60c4767db60ae5",
+    "url": "/img/e1.ea963a2d.png"
+  },
+  {
+    "revision": "06e12ee6d1dbcabbb8968a36bf5d5420",
+    "url": "/img/g2.06e12ee6.jpg"
+  },
+  {
+    "revision": "e5999be25801ced6c2cd7f5a16ac3736",
+    "url": "/img/g1.e5999be2.jpg"
+  },
+  {
+    "revision": "1f452163584e17f7c86af6788be8aeca",
+    "url": "/img/g3.1f452163.jpg"
+  },
+  {
+    "revision": "59fbe8de73ee00067372e4d71f6df7c6",
+    "url": "/img/asd.59fbe8de.png"
+  },
+  {
+    "revision": "c2e6c6dd9afc3df49362ff758d42aa73",
+    "url": "/img/2.c2e6c6dd.png"
+  },
+  {
+    "revision": "6fa1c8cf92ec75b2f2d5c178d63e77a4",
+    "url": "/img/3.6fa1c8cf.png"
+  },
+  {
+    "revision": "59fbe8de73ee00067372e4d71f6df7c6",
+    "url": "/img/logo.59fbe8de.png"
+  },
+  {
+    "revision": "bf9cc87b0c3f36c0ab8703cd9bf827ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "031e1d9fb9b54d7eb752f60fad70a7c3",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,10 +14,10 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.077eb3870ae96bd96deac583e0770c1c.js"
+  "/precache-manifest.3ad3e92bab06e240afb6ff9791a85129.js"
 );
 
-workbox.core.setCacheNameDetails({prefix: "iicwebsit"});
+workbox.core.setCacheNameDetails({prefix: "iicwebsite"});
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
